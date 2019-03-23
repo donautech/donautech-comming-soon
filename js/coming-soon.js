a4eb877f72cceafd1063fc195f6bfb6cbdb811c6
dmitrychaban <dmitrychaban@yahoo.com>
2019-03-23 10:34:44 +0200

    Added background related to Donau Changed styles

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -2,5 +2,18 @@
   "use strict"; // Start of use strict
 
   // No JS
+    var i = 0;
+    var txt = 'Software engineering without corruption, underpayment and misunderstandings';
+    var speed = 50;
+
+    function printCredo() {
+        if (i < txt.length) {
+            document.getElementById("mb-5").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(printCredo, speed);
+        }
+    }
+
+    printCredo()
 
 })(jQuery); // End of use strict
